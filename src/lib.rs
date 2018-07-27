5d0683ce9440c6423549ff24289bbae211ab8369
lorinc <serfozo.lorinc@gmail.com>
2018-07-27 23:00:07 +0200

    loading and saving gray-scale pixels

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+extern crate png;
+use png::HasParameters;
+use std::fs::File;
+use std::io::BufWriter;
+
 pub struct Pixels {
     pub width: usize,
     pub height: usize,
@@ -20,4 +25,36 @@ impl Pixels {
             data
         }
     }
+}
+
+pub fn load_png_to_pixels(png_path: &str) -> Pixels {
+    let decoder = png::Decoder::new(File::open(png_path).unwrap());
+    let (info, mut reader) = decoder.read_info().unwrap();
+    let mut buf = vec![0; info.buffer_size()];
+    reader.next_frame(&mut buf).unwrap();
+
+    let mut data = vec![0.0; (info.width * info.height) as usize];
+
+    for index in 0..data.len() {
+        let red = buf[index*3] as f32 / 255.0;
+        let green = buf[index*3 + 1] as f32 / 255.0;
+        let blue = buf[index*3 + 2] as f32 / 255.0;
+        data[index] = 0.2126*red + 0.7152*green + 0.0722*blue;
+    }
+
+    Pixels::new_with_data(info.width as usize, info.height as usize, data)
+}
+
+pub fn save_pixels_to_png(pixels: &Pixels, png_path: &str) {
+    let file = File::create(png_path).unwrap();
+    let ref mut w = BufWriter::new(file);
+    let mut encoder = png::Encoder::new(w, pixels.width as u32, pixels.height as u32);
+    encoder.set(png::ColorType::Grayscale).set(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+
+    let mut data = vec![0; pixels.data.len()];
+    for index in 0..data.len() {
+        data[index] = (pixels.data[index] * 255.0) as u8;
+    }
+    writer.write_image_data(&data).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate disparity;
+
 fn main() {
-    println!("Hello, world!");
+    let px = disparity::load_png_to_pixels("im0.png");
+    disparity::save_pixels_to_png(&px, "out.png");
 }
