cf6c95bbac681d7c2a5097cc806fd52d42ac20ca
lorinc <serfozo.lorinc@gmail.com>
2018-07-28 18:14:08 +0200

    WINDOW and MAX_DISP are constants

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate disparity;
 extern crate time;
 
+const WINDOW: i32 = 9;
+const MAX_DISP: usize = 65;
+
 fn main() {
-    let window = 9;
-    let max_disp = 65;
     let l_px = disparity::load_png_to_pixels("im0.png");
     let r_px = disparity::load_png_to_pixels("im1.png");
-    let mean = disparity::mean_filter(&l_px, window);
-    let std = disparity::std_filter(&l_px, window);
+    let mean = disparity::mean_filter(&l_px, WINDOW);
+    let std = disparity::std_filter(&l_px, WINDOW);
     let start_time = time::now();
-    let depth = disparity::best_disp_map(&l_px, &r_px, window, max_disp);
+    let depth = disparity::best_disp_map(&l_px, &r_px, WINDOW, MAX_DISP);
     println!("depth map calculation took {} us", (time::now() - start_time).num_microseconds().unwrap());
     disparity::save_pixels_to_png(&l_px, "px.png");
     disparity::save_pixels_to_png(&mean, "mean.png");
