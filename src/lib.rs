f173907149e5fc442ba811745870c3b1e040066c
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2019-01-26 16:34:07 +0100

    use rayon for parallelism

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 extern crate png;
+extern crate rayon;
 use png::HasParameters;
+use rayon::prelude::*;
+use std::cmp::{max, min};
 use std::fs::File;
 use std::io::BufWriter;
-use std::cmp::{min, max};
-use std::thread;
-use std::sync::Arc;
 
 type Range = std::ops::Range<i32>;
 
@@ -16,11 +16,11 @@ pub struct Pixels {
 
 impl Pixels {
     fn new_with_data(width: usize, height: usize, data: Vec<f32>) -> Pixels {
-        assert_eq!(data.len(), width*height);
+        assert_eq!(data.len(), width * height);
         Pixels {
             width,
             height,
-            data
+            data,
         }
     }
 
@@ -32,8 +32,8 @@ impl Pixels {
 }
 
 fn window_ranges(cx: i32, cy: i32, w: i32) -> (Range, Range) {
-    let d = w/2;
-    (cx-d..cx+d+1, cy-d..cy+d+1)
+    let d = w / 2;
+    (cx - d..cx + d + 1, cy - d..cy + d + 1)
 }
 
 fn mean_window(pixels: &Pixels, cx: i32, cy: i32, w: i32) -> f32 {
@@ -44,7 +44,7 @@ fn mean_window(pixels: &Pixels, cx: i32, cy: i32, w: i32) -> f32 {
             sum += pixels.get(col, row)
         }
     }
-    sum / (w*w) as f32
+    sum / (w * w) as f32
 }
 
 fn std_window(pixels: &Pixels, cx: i32, cy: i32, w: i32) -> f32 {
@@ -54,21 +54,27 @@ fn std_window(pixels: &Pixels, cx: i32, cy: i32, w: i32) -> f32 {
     for row in yr {
         for col in xr.clone() {
             let val = pixels.get(col, row);
-            sum += (val - mean)*(val - mean);
+            sum += (val - mean) * (val - mean);
         }
     }
     sum.sqrt()
 }
 
-fn zncc_window(l_pix: &Pixels, r_pix: &Pixels, cx: i32, cy: i32, w: i32, disp: i32, l_mean: f32)
-    -> f32
-{
+fn zncc_window(
+    l_pix: &Pixels,
+    r_pix: &Pixels,
+    cx: i32,
+    cy: i32,
+    w: i32,
+    disp: i32,
+    l_mean: f32,
+) -> f32 {
     let (xr, yr) = window_ranges(cx, cy, w);
     let r_mean = mean_window(&r_pix, cx, cy, w);
     let mut sum = 0.0;
     for row in yr {
         for col in xr.clone() {
-            sum += (l_pix.get(col, row) - l_mean)*(r_pix.get(col - disp, row) - r_mean);
+            sum += (l_pix.get(col, row) - l_mean) * (r_pix.get(col - disp, row) - r_mean);
         }
     }
     sum / std_window(&l_pix, cx, cy, w) / std_window(&r_pix, cx - disp, cy, w)
@@ -88,46 +94,27 @@ fn best_zncc(l_pix: &Pixels, r_pix: &Pixels, cx: i32, cy: i32, w: i32, max_d: us
     best_disp
 }
 
-fn best_disp_part(l_pix: &Pixels, r_pix: &Pixels, w: i32,
-                  max_disp: usize, start_row: usize, end_row: usize) -> Vec<f32>
-{
-    let mut data = vec![0.0; (end_row - start_row)*l_pix.width];
-    for row in start_row..end_row {
-        for col in 0..l_pix.width {
-            let idx = col + (row - start_row)*l_pix.width;
-            let best_disp = best_zncc(&l_pix, &r_pix, col as i32, row as i32, w, max_disp);
-            data[idx as usize] = best_disp as f32 / max_disp as f32;
-        }
-    }
-    data
-}
-
-pub fn best_disp_map(l_pix: Pixels, r_pix: Pixels, w: i32, max_disp: usize, threads: usize)
-    -> Pixels
-{
+pub fn best_disp_map(
+    l_pix: Pixels,
+    r_pix: Pixels,
+    w: i32,
+    max_disp: usize,
+    threads: usize,
+) -> Pixels {
     assert_eq!(l_pix.width, r_pix.width);
     assert_eq!(l_pix.height, r_pix.height);
     assert_eq!(l_pix.height % threads, 0);
-    let rows = l_pix.height/threads;
-    let mut new_data = Vec::new();
-
-    let mut handles = Vec::new();
-    let l_pix = Arc::new(l_pix);
-    let r_pix = Arc::new(r_pix);
-
-    for i in 0..threads {
-        let start_row = i*rows;
-        let end_row = (i + 1)*rows;
-        let l_clone = l_pix.clone();
-        let r_clone = r_pix.clone();
-        handles.push(thread::spawn(move || {
-            return best_disp_part(&l_clone, &r_clone, w, max_disp, start_row, end_row);
-        }));
-    }
-    for handle in handles {
-        let mut data_part = handle.join().unwrap();
-        new_data.append(&mut data_part);
-    }
+    let index_to_xy = |index: usize| (index % l_pix.width, index / l_pix.width);
+    let new_data: Vec<f32> = l_pix
+        .data
+        .par_iter()
+        .enumerate()
+        .map(|(index, _)| {
+            let (col, row) = index_to_xy(index);
+            return best_zncc(&l_pix, &r_pix, col as i32, row as i32, w, max_disp) as f32
+                / max_disp as f32;
+        })
+        .collect();
     Pixels::new_with_data(l_pix.width, l_pix.height, new_data)
 }
 
@@ -140,10 +127,10 @@ pub fn load_png_to_pixels(png_path: &str) -> Pixels {
     let mut data = vec![0.0; (info.width * info.height) as usize];
 
     for index in 0..data.len() {
-        let red = buf[index*3] as f32 / 255.0;
-        let green = buf[index*3 + 1] as f32 / 255.0;
-        let blue = buf[index*3 + 2] as f32 / 255.0;
-        data[index] = 0.2126*red + 0.7152*green + 0.0722*blue;
+        let red = buf[index * 3] as f32 / 255.0;
+        let green = buf[index * 3 + 1] as f32 / 255.0;
+        let blue = buf[index * 3 + 2] as f32 / 255.0;
+        data[index] = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
     }
 
     Pixels::new_with_data(info.width as usize, info.height as usize, data)
@@ -153,7 +140,9 @@ pub fn save_pixels_to_png(pixels: &Pixels, png_path: &str) {
     let file = File::create(png_path).unwrap();
     let ref mut w = BufWriter::new(file);
     let mut encoder = png::Encoder::new(w, pixels.width as u32, pixels.height as u32);
-    encoder.set(png::ColorType::Grayscale).set(png::BitDepth::Eight);
+    encoder
+        .set(png::ColorType::Grayscale)
+        .set(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
 
     let mut data = vec![0; pixels.data.len()];
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate time;
 
 const WINDOW: i32 = 9;
 const MAX_DISP: usize = 65;
-const THREADS : usize = 4;
+const THREADS: usize = 4;
 
 fn main() {
     let l_px = disparity::load_png_to_pixels("im0.png");
@@ -11,7 +11,9 @@ fn main() {
 
     let start_time = time::now();
     let depth = disparity::best_disp_map(l_px, r_px, WINDOW, MAX_DISP, THREADS);
-    println!("depth map calculation took {} us",
-             (time::now() - start_time).num_microseconds().unwrap());
+    println!(
+        "depth map calculation took {} us",
+        (time::now() - start_time).num_microseconds().unwrap()
+    );
     disparity::save_pixels_to_png(&depth, "depth.png");
 }
